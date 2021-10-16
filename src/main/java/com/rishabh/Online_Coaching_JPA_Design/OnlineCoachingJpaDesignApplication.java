8bea7c29685cc2673261764845db79f2547adcc1
Rishabh <rishabhg@adobe.com>
2021-10-16 06:42:09 +0530

    First

---
+++ src/main/java/com/rishabh/Online_Coaching_JPA_Design/OnlineCoachingJpaDesignApplication.java
@@ -0,0 +1,13 @@
+package com.rishabh.Online_Coaching_JPA_Design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCoachingJpaDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCoachingJpaDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishabh/Online_Coaching_JPA_Design/OnlineCoachingJpaDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabh.Online_Coaching_JPA_Design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoachingJpaDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
